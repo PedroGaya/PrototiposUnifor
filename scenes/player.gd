edfffb10222614b3c243fb88aa0aa15ab9f648c0
PedroGayai <pedrohenriquegaya@gmail.com>
2024-03-01 16:56:18 -0300

    aula 3.2 parcial

--- scenes/player.gd
+++ scenes/player.gd
@@ -5,6 +5,18 @@ var direction = Vector2.ZERO
 
 @onready var collision_rect = $CollisionShape2D
 
+var bounding_box_width
+var start_bound
+var end_bound
+
+func _ready():
+	bounding_box_width = collision_rect.shape.get_rect().size.x
+	var screen = get_viewport().get_visible_rect()
+	var camera = get_viewport().get_camera_2d()
+	
+	start_bound = (camera.position.x - screen.size.x) / 2
+	end_bound = (camera.position.x + screen.size.x) / 2
+
 func _process(delta):
 	var input = Input.get_axis("left", "right")
 	if input > 0:
@@ -13,6 +25,14 @@ func _process(delta):
 		direction = Vector2.LEFT
 	else:
 		direction = Vector2.ZERO
-		
+	
 	var delta_movement = input * speed * delta
-	position.x += delta_movement 
+
+	if (
+		position.x + delta_movement < start_bound + bounding_box_width * transform.get_scale().x or
+		position.x + delta_movement > end_bound - bounding_box_width * transform.get_scale().x
+	):
+		return
+		
+	position.x += delta_movement
+
