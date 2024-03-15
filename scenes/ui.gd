0e41e58313ead20d802626427eb65bcd77799f1f
PedroGayai <pedrohenriquegaya@gmail.com>
2024-03-15 15:10:34 -0300

    final

--- scenes/ui.gd
+++ scenes/ui.gd
@@ -3,8 +3,8 @@ extends CanvasLayer
 @onready var points_label = $MarginContainer/Points
 @onready var lives_container = $MarginContainer/Lives
 @onready var game_over_container = $MarginContainer/GameOver
-@onready var game_over_label = %MarginContainer/GameOver/VBoxContainer/Label
-@onready var game_over_button = %MarginContainer/GameOver/VBoxContainer/Button
+@onready var game_over_label = %GameOverLabel
+@onready var game_over_button = %GameOverButton
 
 @onready var invader_spawner = $"../InvaderSpawner" as InvaderSpawner
 @onready var life_manager = $"../LifeManager" as LifeManager
@@ -23,7 +23,29 @@ func _ready():
 		var life_texture_rect = TextureRect.new()
 		life_texture_rect.expand_mode = TextureRect.EXPAND_KEEP_SIZE
 		life_texture_rect.custom_minimum_size = Vector2(40, 25)
+		life_texture_rect.texture_filter = CanvasItem.TEXTURE_FILTER_NEAREST
+		life_texture_rect.texture = life_texture
 		life_texture_rect.expand_mode = TextureRect.EXPAND_KEEP_SIZE
-		life_texture_rect.expand_mode = TextureRect.EXPAND_KEEP_SIZE
-		life_texture_rect.expand_mode = TextureRect.EXPAND_KEEP_SIZE
-		life_texture_rect.expand_mode = TextureRect.EXPAND_KEEP_SIZE
+		lives_container.add_child(life_texture_rect)
+
+func _on_points_increased(points: int):
+		points_label.text = 'SCORE: %d' % points
+
+func _on_life_lost(lives: int):
+	if lives != 0:
+		var life_texture_rect = lives_container.get_child(lives)
+		life_texture_rect.queue_free()
+	else:
+		_on_game_lost()
+
+func _on_game_lost():
+	game_over_container.visible = true
+
+func _on_game_won():
+	game_over_container.visible = true
+	game_over_label.text = "VICTORY"
+	game_over_label.add_theme_color_override("font_color", Color.YELLOW)
+
+
+func _on_button_pressed():
+	get_tree().reload_current_scene()
